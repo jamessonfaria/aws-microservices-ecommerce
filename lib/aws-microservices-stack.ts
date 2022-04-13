2bde43d2e7ddc55f365b014ef916bb9c5192a85d
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-13 15:14:25 -0300

    Create table in aws dynamodb by IaC

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,11 +1,21 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    
+    const productTable = new Table(this, 'product', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+
+
   }
 }
