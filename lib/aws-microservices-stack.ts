fea7418f4f73e2a07f2bd4b4fbc404b285a1a04f
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-13 16:08:27 -0300

    Add AWS lambda function for product microservice

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,6 +1,9 @@
 import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { join } from 'path';
 
 export class AwsMicroservicesStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,6 +19,24 @@ export class AwsMicroservicesStack extends Stack {
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
 
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: [
+          'aws-sdk'
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMODB_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_14_X
+    };
 
+    const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
+      entry: join(__dirname, `/../src/product/index.js`),
+      ...nodeJsFunctionProps,
+    });
+
+    productTable.grantReadWriteData(productFunction);
   }
 }
