f7dbe9805ce817c68e90f31c44d8997a8302b1e1
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-14 16:50:32 -0300

    Added ddbClient for dynamoDB and dependencies, adjust index to method GET

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,20 @@
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
+
+    switch (event.httpMethod) {
+      case "GET":
+        if (event.pathParameters != null) {
+          body = await getProduct(event.pathParameters.id); // GET product/{id}
+        } else {
+          body = await getAllProducts(); // GET product
+        }
+        break;        
+    }     
+
+
     return {
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain" },
-      body: `Hello from Product ! You've hit ${event.path}\n`
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: `Hello from Product ! You've hit ${event.path}\n`
     };
 };
