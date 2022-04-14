47846d0117e73f4276225ee3cb9402acbff7e1e3
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-14 17:44:53 -0300

    Added methods getAllProducts and getProduct

--- src/product/index.js
+++ src/product/index.js
@@ -1,3 +1,7 @@
+import { GetItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall, marshall } from "@aws-sdk/util-dynamodb";
+import { ddbClient } from "./ddbClient";
+
 exports.handler = async function(event) {
     console.log("request:", JSON.stringify(event, undefined, 2));
 
@@ -18,3 +22,42 @@ exports.handler = async function(event) {
         body: `Hello from Product ! You've hit ${event.path}\n`
     };
 };
+
+const getProduct = async (productId) => {
+  console.log("getProduct");
+
+  try {
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME,
+      Key: marshall({ id: productId })
+    };
+
+    const { Item } = await ddbClient.send(new GetItemCommand(params));
+
+    console.log(Item);
+    return (Item) ? unmarshall(Item) : {};
+
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+const getAllProducts = async () => {
+  console.log("getAllProducts");
+  try {
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE_NAME
+    };
+
+    const { Items } = await ddbClient.send(new ScanCommand(params));
+
+    console.log(Items);
+    return (Items) ? Items.map((item) => unmarshall(item)) : {};
+
+  } catch(e) {
+    console.error(e);
+    throw e;
+  }
+}
