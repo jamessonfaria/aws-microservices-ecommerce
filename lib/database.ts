0e910fe9c8c6379abad50691279125e22f3bd480
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-18 16:55:54 -0300

    Create basket table

--- lib/database.ts
+++ lib/database.ts
@@ -4,14 +4,15 @@ import { Construct } from "constructs";
 
 export class SwnDatabase extends Construct {
     public readonly productTable: ITable;
+    public readonly basketTable: ITable;
 
     constructor(scope: Construct, id: string){
         super(scope, id);
 
         const productTable = new Table(this, 'product', {
             partitionKey: {
-            name: 'id',
-            type: AttributeType.STRING
+                name: 'id',
+                type: AttributeType.STRING
             },
             tableName: 'product',
             removalPolicy: RemovalPolicy.DESTROY,
@@ -19,5 +20,17 @@ export class SwnDatabase extends Construct {
         });
 
         this.productTable = productTable;
+
+        const basketTable = new Table(this, 'basket', {
+            partitionKey: {
+                name: 'userName',
+                type: AttributeType.STRING
+            },
+            tableName: 'basket',
+            removalPolicy: RemovalPolicy.DESTROY,
+            billingMode: BillingMode.PAY_PER_REQUEST,
+        });
+
+        this.basketTable = basketTable;
     }
 }
