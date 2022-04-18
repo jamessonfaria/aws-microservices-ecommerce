f6708abed0cac64f337cf8f4eb2daf0ae27fa02d
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-18 15:31:51 -0300

    Remove imports unused

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,9 +1,6 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-import { join } from 'path';
 import { SwnDatabase } from './database';
 import { SwnMicroservices } from './microservice';
 
@@ -12,7 +9,7 @@ export class AwsMicroservicesStack extends Stack {
     super(scope, id, props);
 
     const database = new SwnDatabase(this, 'Database');
-    
+
     const microservices = new SwnMicroservices(this, 'Microservices', {
       productTable: database.productTable
     });
