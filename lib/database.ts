d717725610db61b1581fb0384f053768a31f2b26
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-19 14:40:16 -0300

    Added new table order

--- lib/database.ts
+++ lib/database.ts
@@ -5,6 +5,7 @@ import { Construct } from "constructs";
 export class SwnDatabase extends Construct {
     public readonly productTable: ITable;
     public readonly basketTable: ITable;
+    public readonly orderTable: ITable;
 
     constructor(scope: Construct, id: string){
         super(scope, id);
@@ -13,6 +14,8 @@ export class SwnDatabase extends Construct {
          this.productTable = this.createProductTable();
          //basket table
          this.basketTable = this.createBasketTable(); 
+         //order table
+         this.orderTable = this.createOrderTable(); 
     }
 
     private createProductTable() : ITable {
@@ -42,4 +45,22 @@ export class SwnDatabase extends Construct {
 
         return basketTable;
     }
+
+    private createOrderTable() : ITable {
+        const orderTable = new Table(this, 'order', {
+            partitionKey: {
+              name: 'userName',
+              type: AttributeType.STRING,
+            },
+            sortKey: {
+              name: 'orderDate',
+              type: AttributeType.STRING,
+            },
+            tableName: 'order',
+            removalPolicy: RemovalPolicy.DESTROY,
+            billingMode: BillingMode.PAY_PER_REQUEST
+        });
+
+        return orderTable;
+      }
 }
