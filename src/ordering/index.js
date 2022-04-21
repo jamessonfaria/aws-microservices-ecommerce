3109b881cfee264d0d00d6c53d32aec57fd04650
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-21 10:55:26 -0300

    Adjust for order microservice catch event source mapping in SQS queue

--- src/ordering/index.js
+++ src/ordering/index.js
@@ -5,17 +5,33 @@ import { ddbClient } from './ddbClient';
 exports.handler = async function (event) {
   console.log("request:", JSON.stringify(event, undefined, 2));
 
-  // TODO - Catch and Process Async EventBridge Invocation and Sync API Gateway Invocation     
-  const eventType = event['detail-type'];
-  if (eventType !== undefined) {
+  if(event.Records != null) {
+    // SQS Invocation
+    await sqsInvocation(event);
+  }
+  else if (event['detail-type'] !== undefined) {
     // EventBridge Invocation
     await eventBridgeInvocation(event);
-
   } else {
     // API Gateway Invocation -- return sync response
     return await apiGatewayInvocation(event);
   }
 };
+
+const sqsInvocation = async (event) => {
+  console.log(`sqsInvocation function. event : "${event}"`);
+  
+  event.Records.forEach(async (record) => {
+    console.log('Record: %j', record);
+    
+    // expected request : { "detail-type\":\"CheckoutBasket\",\"source\":\"com.swn.basket.checkoutbasket\", "detail\":{\"userName\":\"swn\",\"totalPrice\":1820, .. }
+    const checkoutEventRequest = JSON.parse(record.body); 
+    
+    // create order item into db
+    await createOrder(checkoutEventRequest.detail); 
+    // detail object should be checkoutbasket json object
+  });
+}
 
 const eventBridgeInvocation = async (event) => {
   console.log(`eventBridgeInvocation function. event : "${event}"`);
